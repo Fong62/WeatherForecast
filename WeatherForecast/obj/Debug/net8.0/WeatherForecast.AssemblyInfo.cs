a2f4ad48a4a7b48cf83942b8e80999fd3b6c1196
Nguyen Hoang Phong <4801104106@student.hcmue.edu.vn>
2025-05-26 19:01:27 +0700

    Initial Commit

--- WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
+++ WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46db341b61a893b0fb89a621337126b0e0fec76")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
