402e2a9735ff9e22517d454c5003b227cc9828b0
Nguyen Hoang Phong <4801104106@student.hcmue.edu.vn>
2025-05-29 17:05:41 +0700

    Initial Commit

--- WeatherForecast.Tests/UnitTest1.cs
+++ WeatherForecast.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using Xunit;
+﻿using Xunit;
 using WeatherForecast.Controllers;
 using Microsoft.Extensions.Logging.Abstractions;
 using System.Linq;
@@ -17,6 +17,23 @@ namespace WeatherForecast.Tests
 
             Assert.NotNull(result);
             Assert.Equal(5, result.Count());
+        }
+
+        [Fact]
+        public void Get_ForecastItemsHaveValidValues()
+        {
+            var logger = NullLogger<WeatherForecastController>.Instance;
+            var controller = new WeatherForecastController(logger);
+
+            var result = controller.Get().ToList();
+
+            Assert.Equal(5, result.Count);
+            foreach (var forecast in result)
+            {
+                Assert.InRange(forecast.TemperatureC, -20, 55);
+                Assert.False(string.IsNullOrWhiteSpace(forecast.Summary));
+                Assert.InRange(forecast.Date, DateTime.Now.AddDays(1).Date, DateTime.Now.AddDays(5).Date);
+            }
         }
     }
 }
